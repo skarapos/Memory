15df952524c7639864cab025aeefcba3653dc537
Sotiris Karapostolakis <sotkar14@gmail.com>
2015-07-18 18:15:21 +0300

    Update cl_card.java

--- cl_card.java
+++ cl_card.java
@@ -3,11 +3,7 @@
  * @author Sotiris Karapostolakis
  */
 public class cl_card extends Card{
-    /**
-     * Constructor για την δημιουργια της κλασσικης καρτας.
-     * @param ide
-     * @param value
-     */
+
     public cl_card(int ide,int value)
     {
         super(ide,value);

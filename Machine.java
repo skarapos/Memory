7f45f21ad7f42c25840147eb2884eed3d95c8015
Sotiris Karapostolakis <sotkar14@gmail.com>
2015-07-18 18:09:52 +0300

    Update Machine.java

--- Machine.java
+++ Machine.java
@@ -4,10 +4,7 @@
  * @author Sotiris Karapostolakis
  */
 public class Machine extends Player {
-    /**
-     * Constructor που δημιουργει το αντικειμενο του υπολογιστη.
-     * @param numb
-     */
+
     public Machine(int numb)
     {
         super(numb);

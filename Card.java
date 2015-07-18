fe578cb07f18dd6b70108e025c06cfc7c255021a
Sotiris Karapostolakis <sotkar14@gmail.com>
2015-07-18 18:07:19 +0300

    Update Card.java

--- Card.java
+++ Card.java
@@ -5,28 +5,18 @@
 public class Card{
     private int id;
     private int value;
-    /**
-     * Constructor γαι την δημιουργια της καρτας.
-     * @param id
-     * @param value
-     */
+
     public Card(int id,int value)
     {
         this.id=id;
         this.value=value;
     }
-    /**
-     *
-     * @return Το id της καρτας.
-     */
+
     public int returnid()
     {
         return id;
     }
-    /**
-     *
-     * @return Την αξια της καρτας.
-     */
+
     public int returnvalue()
     {
         return value;
